079a8809103897c204ffde763eb8546bb5b00ee5
Aleks164 <alekseylehamail.ru>
2022-01-27 20:04:47 +0300

    barely probably  almost done2

--- src/curryr.ts
+++ src/curryr.ts
@@ -1,10 +1,10 @@
 
 export function curry(func: (...arg: number[]) => number): any {
-  return function curried(...args: number[]) {
+  return function curried(this: any, ...args: number[]) {
     if (args.length >= func.length) {
       return func.apply(this, args);
     }
-    return function (...args2: number[]) {
+    return (...args2: number[]) => {
       return curried.apply(this, args.concat(args2));
     };
   };
